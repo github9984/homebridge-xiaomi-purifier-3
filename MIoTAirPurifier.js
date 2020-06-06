d2a22d8127b0c6616dd1f0f033fc8549c64a1816
devguy <>
2020-06-07 00:12:12 +1000

    set favorite_level siid piid

--- MIoTAirPurifier.js
+++ MIoTAirPurifier.js
@@ -10,7 +10,7 @@ class MIoTAirPurifier extends MIoTDevice {
             'aqi'            : [ 3,  6], // PM2.5 Density : 0-600 1
             'humidity'       : [ 3,  7], // Relative Humidity: 0-100(Percentage)
             'temp'           : [ 3,  8], // Temperature: -40-125 0.1
-            'favorite_level' : [ 10, 3],
+            'favorite_level' : [ 10, 10],
             'filter_level'   : [ 4,  3], // Filter Live Level : 0-100(Percentage)
             'child_lock'     : [ 7,  1], // Physical Control Locked : bool
             'led_brightness' : [ 6,  1], // 0 (bright), 1 (dim), 2 (off)            
@@ -54,13 +54,13 @@ class MIoTAirPurifier extends MIoTDevice {
     
     getSpeed() {
         var favorite_level = this.get('favorite_level');
-        const rotationSpeed = (favorite_level / 16) * 100;
+        const rotationSpeed = (favorite_level / 14) * 100;
 
-        return Math.round(rotationSpeed);
+        return Math.round(rotationSpeed);        
     }
 
     setSpeed(speed) {
-        const favorite_level = (speed / 100) * 16;
+        const favorite_level = (speed / 100) * 14;
         this.set('favorite_level', favorite_level);
     }
 }
